1771f14af6687c8f01a1b6fe1a947f11b934b1a6
DokyungYou <125132565+DokyungYou@users.noreply.github.com>
2024-07-22 22:17:11 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.06 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,12 +1,9 @@
 class Solution {
     public String solution(String my_string) {
-        char[] charArr = my_string.toCharArray();
         StringBuilder sb = new StringBuilder();
-        
-        for(int i = charArr.length -1 ; i >= 0; i--){
-            sb.append(charArr[i]);
-        }   
-        
+        sb.append(my_string);
+        sb.reverse();
+
         return sb.toString();
     }
 }
