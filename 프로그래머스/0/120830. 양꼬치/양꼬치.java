7598fced684feb20c9b28f25cc3fb5cb0def5e8c
DokyungYou <125132565+DokyungYou@users.noreply.github.com>
2024-07-23 22:03:39 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,12 +1,5 @@
 class Solution {
     public int solution(int n, int k) {
-        int payment = (12000 * n) + (2000 * k);
-        
-        if(n / 10 == 0){
-            return payment;
-        }else{
-            return payment - (2000 * (n/10));
-        }
-        
+        return (12000 * n) + (2000 * k) - (2000 * (n/10));
     }
 }
