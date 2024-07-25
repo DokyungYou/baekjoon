892a66e77a1b5182e53010942e267f532f8b9f74
DokyungYou <125132565+DokyungYou@users.noreply.github.com>
2024-07-25 22:32:44 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/README.md
+++ 프로그래머스/0/120837. 개미 군단/README.md
@@ -1,10 +1,10 @@
 # [level 0] 개미 군단 - 120837 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120837) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120837#) 
 
 ### 성능 요약
 
-메모리: 68.5 MB, 시간: 0.03 ms
+메모리: 75.9 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 25일 22:27:30
+2024년 07월 25일 22:32:42
 
 ### 문제 설명
 
--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -3,20 +3,17 @@ class Solution {
  
         int cnt = 0;
 
-        if(hp >= 5){
-            cnt += hp / 5; 
-            hp %= 5;
-        }
+        // 굳이 반복문이나 조건문이 필요없다.
+
+        cnt += hp / 5; // 미만이라면, cnt += 0 , hp는 기존 수 유지
+        hp %= 5;
         
-        if(hp >= 3){
-            cnt += hp / 3; 
-            hp %= 3;
-        }
+        cnt += hp / 3; 
+        hp %= 3;
         
-        if(hp >= 1){
-            cnt += hp / 1; 
-            hp %= 1;
-        }
+        cnt += hp / 1; 
+        hp %= 1;
+
 
         return cnt;
     }
