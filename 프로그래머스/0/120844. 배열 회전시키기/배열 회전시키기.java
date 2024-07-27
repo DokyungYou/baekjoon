59ddcd548fb0ae34a62a5665f152dba86f24ca62
DokyungYou <125132565+DokyungYou@users.noreply.github.com>
2024-07-27 18:47:02 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -1,26 +1,30 @@
-import java.util.ArrayDeque;
 class Solution {
     public int[] solution(int[] numbers, String direction) {
-        int[] answer = new int[numbers.length];
-        ArrayDeque<Integer> deque = new ArrayDeque<>();
-
-        for (int i = 0; i < numbers.length; i++) {
-            deque.add(numbers[i]);
-        }
-
+        int left ,right = 0;
+        
         if(direction.equals("right")){
-            int item= deque.removeLast();
-            deque.addFirst(item);
+    
+            left = numbers[0];
+            numbers[0] = numbers[numbers.length - 1];
+            for(int i = 1; i < numbers.length; i++){
+                
+                right = numbers[i];
+                numbers[i] = left;
+                left = right;
+            }
         }else{
-            int item = deque.removeFirst();
-            deque.addLast(item);
-        }
-        Integer[] num =deque.toArray(new Integer[0]);
+            
+            right = numbers[numbers.length - 1];
+            numbers[numbers.length - 1] = numbers[0];
+            for(int i = numbers.length - 2; i >= 0; i--){
 
-        for (int i = 0; i < num.length; i++) {
-            answer[i]=num[i];
+                left = numbers[i];
+                numbers[i] = right;
+                right = left;
+                
+            }
         }
-
-        return answer;
+        
+        return numbers;
     }
 }
