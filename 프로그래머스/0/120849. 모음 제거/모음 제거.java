7fd34916fddc84310e7978c260d820d22fbdb84b
DokyungYou <125132565+DokyungYou@users.noreply.github.com>
2024-07-29 14:20:06 +0900

    [level 0] Title: 모음 제거, Time: 0.07 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -3,14 +3,19 @@ class Solution {
         
         char[] vowel = {'a', 'e', 'i', 'o', 'u'};
         
-        for(char c : vowel){
-            for(int i = 0; i < my_string.length() ; i++){
-                my_string = my_string.replace(String.valueOf(c), "");
-            } 
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < my_string.length(); i++){
+            
+            sb.append(my_string.charAt(i));
+            for(int j = 0; j < vowel.length ; j++){
+                if(my_string.charAt(i) == vowel[j]){
+                    sb.deleteCharAt(sb.length()-1);
+                    break;
+                }
+            }
         }
         
         
-        
-        return my_string;
+        return sb.toString();
     }
 }
