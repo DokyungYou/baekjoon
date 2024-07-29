e8ccd541dbfee8833de6b057a2505f77a91626a2
DokyungYou <125132565+DokyungYou@users.noreply.github.com>
2024-07-29 11:51:35 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.05 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,8 +1,23 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[] numbers) {
-        Arrays.sort(numbers);
-        return numbers[numbers.length - 1] * numbers[numbers.length - 2];
+
+       int[] max = {-1, -1};
+        for(int i = 0; i < numbers.length ; i++){
+            if(numbers[i] < max[1]){
+                continue;
+            }
+            
+            if(numbers[i] > max[0]){
+                max[1] =max[0];
+                max[0] = numbers[i];
+                
+                continue;
+            }
+            
+            if(numbers[i] > max[1]){
+                max[1] = numbers[i];
+            }
+        }
+        return max[0] * max[1];
     }
 }
