311c708b104985d305e2e39716666c08d87eea4e
DokyungYou <125132565+DokyungYou@users.noreply.github.com>
2024-07-29 11:15:49 +0900

    [level 0] Title: 합성수 찾기, Time: 0.03 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int n) {
        int count = 0;
         for(int i = 4; i <= n ; i++){ //  합성수의 가장 작은 수부터 시작
-             for(int j = 2; j < i ; j++){
+             for(int j = 2; j <= i/2 ; j++){
                 if(i % j == 0){
                     count++;
                     break;
