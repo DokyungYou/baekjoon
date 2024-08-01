522f7e522d872956c0e81ed175e9397d7744d92d
DokyungYou <125132565+DokyungYou@users.noreply.github.com>
2024-08-01 19:25:01 +0900

    [level 0] Title: 약수 구하기, Time: 0.32 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -3,24 +3,31 @@ import java.util.*;
 class Solution {
     public int[] solution(int n) {
         
-        int[] arr = new int[n];
-        arr[0] = 1;
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
         
-        int idx = 1;
+        
         if(n % 2 != 0){
             for(int i = 3; i <= n; i+=2){
                 if(n % i == 0){
-                    arr[idx++] = i;
+                    list.add(i);
                 }
             }
         }else{
             for(int i = 2; i <= n; i++){
                 if(n % i == 0){
-                    arr[idx++] = i;
+                    list.add(i);
                 }
             }
         }
         
-        return Arrays.copyOf(arr, idx);
+        //return list.stream().mapToInt(x -> x).toArray();  //느림
+        
+        int[] result = new int[list.size()];
+        for(int i = 0; i < result.length; i++){
+            result[i] = list.get(i);
+        }
+        
+        return result;
     }
 }
