2a64009dc6f0265c237f2017daf0c2dea9c15d01
DokyungYou <125132565+DokyungYou@users.noreply.github.com>
2024-08-05 02:22:41 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.04 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,5 +1,20 @@
 class Solution {
     public int solution(String str1, String str2) {
-        return str1.contains(str2) ? 1 : 2;
+        
+        int count = 0;
+        int idx = 0;
+        for(int i = 0; i < str1.length(); i++){
+            if(str1.charAt(i) == str2.charAt(idx++)){
+                count++;
+            }else{
+                count = 0;
+                idx = 0;
+            }
+            
+            if(count >= str2.length()){
+                return 1;
+            }
+        }
+        return 2;
     }
 }
